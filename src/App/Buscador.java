0705fa9b834da3e27e8836d01ea593c8f7ad03e9
uriigrao <oriol.gm375@gmail.com>
2020-02-14 13:48:19 +0100

    add last point

--- src/App/Buscador.java
+++ src/App/Buscador.java
@@ -1,21 +1,54 @@
 package App;
 
-import Models.Incidencias;
-import java.io.File;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.xpath.*;
+import java.io.FileWriter;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.InputSource;
+import java.io.*;
+import static utils.Colors.printBlue;
+import static utils.Colors.printRed;
 
 /**
  *
  * @author uriishii
  */
 public class Buscador {
-    public static void main(String[] args) throws JAXBException {
-        JAXBContext context =  JAXBContext.newInstance(Incidencias.class);
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        Incidencias incidencias = (Incidencias) unmarshaller.unmarshal(new File("Documents/incidencias.xml"));
-        
-        System.out.println(incidencias.getIncidencias());
-    }
+	public static void main(String[] args) {
+		XPath xpath;
+		XPathExpression exp;
+		Document XMLDoc;
+
+		openFileXML();
+		String result = executeXpath("///destino[.='jramirez']");
+
+		printBLUE(result);
+	}
+
+	public int openFileXML() {
+		try {
+			xpath = XPathFactory.newInstance().newXPath();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			XMLDoc = factory.newDocumentBuilder().parse(
+				new InputSource(new FileInputStream("Documents/incidencias.xml")));
+			return 0;
+		}
+		catch(Exception ex) {
+			printRED("Error: " + ex.toString());
+			return -1;
+		}
+	}
+
+	public String executeXpath(String consulta){
+		String salida = "";
+		try {
+			exp = xpath.compite(consulta);
+			NodeList nodeList = (NodeList) exp.evaluate(XMLDoc, XPathConstants.NODESET);
+			for(int i = 0; i < nodeList.getLength(); i++) {
+				salida = salida + "\n" + nodeList.item(i).getChildNodes().item(0).getNodeValue();
+			}
+			return salida;
+		} catch(Exception ex) {
+			return "Error mostrar Datos: " + ex.toString();
+		}
+	}
 }
