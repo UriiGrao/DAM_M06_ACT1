7095414ed0c5389bba4d5f5a56b06782d65f367c
uriishii <>
2020-02-14 17:56:01 +0100

    finish

--- src/App/Buscador.java
+++ src/App/Buscador.java
@@ -1,10 +1,11 @@
 package App;
 
 import javax.xml.xpath.*;
-import java.io.FileWriter;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.xml.sax.InputSource;
 import java.io.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import static utils.Colors.printBlue;
 import static utils.Colors.printRed;
 
@@ -13,42 +14,42 @@ import static utils.Colors.printRed;
  * @author uriishii
  */
 public class Buscador {
-	public static void main(String[] args) {
-		XPath xpath;
-		XPathExpression exp;
-		Document XMLDoc;
 
-		openFileXML();
-		String result = executeXpath("///destino[.='jramirez']");
+    public static XPath xpath;
+    public static XPathExpression exp;
+    public static Document XMLDoc;
 
-		printBLUE(result);
-	}
+    public static void main(String[] args) {
 
-	public int openFileXML() {
-		try {
-			xpath = XPathFactory.newInstance().newXPath();
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			XMLDoc = factory.newDocumentBuilder().parse(
-				new InputSource(new FileInputStream("Documents/incidencias.xml")));
-			return 0;
-		}
-		catch(Exception ex) {
-			printRED("Error: " + ex.toString());
-			return -1;
-		}
-	}
+        openFileXML();
+        String result = executeXpath("//incidencia[./destino = 'jramirez' ]");
+        printBlue(result);
+    }
 
-	public String executeXpath(String consulta){
-		String salida = "";
-		try {
-			exp = xpath.compite(consulta);
-			NodeList nodeList = (NodeList) exp.evaluate(XMLDoc, XPathConstants.NODESET);
-			for(int i = 0; i < nodeList.getLength(); i++) {
-				salida = salida + "\n" + nodeList.item(i).getChildNodes().item(0).getNodeValue();
-			}
-			return salida;
-		} catch(Exception ex) {
-			return "Error mostrar Datos: " + ex.toString();
-		}
-	}
+    public static int openFileXML() {
+        try {
+            xpath = XPathFactory.newInstance().newXPath();
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            XMLDoc = factory.newDocumentBuilder().parse(
+                    new InputSource(new FileInputStream("Documents/incidencias.xml")));
+            return 0;
+        } catch (Exception ex) {
+            printRed("Error: " + ex.toString());
+            return -1;
+        }
+    }
+
+    public static String executeXpath(String consulta) {
+        String salida = "";
+        try {
+            exp = xpath.compile(consulta);
+            NodeList nodeList = (NodeList) exp.evaluate(XMLDoc, XPathConstants.NODESET);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                salida = salida + "\n" + nodeList.item(i).getTextContent();
+            }
+            return salida;
+        } catch (Exception ex) {
+            return "Error mostrar Datos: " + ex.toString();
+        }
+    }
 }
